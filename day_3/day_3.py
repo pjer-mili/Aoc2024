a11eb7a23548eba781c0f0467b50b0f2142b5e5d
PetarMilisavljevic <milisavljevicpetar6@gmail.com>
2024-12-03 15:23:44 +0100

    day 3 part 2

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -3,13 +3,24 @@ import re
 
 def main():
     sum = 0
-    pattern = re.compile(r"mul\(\d{1,3},\d{1,3}\)")
+    pattern = re.compile(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)")
+
     with open("./input.txt", "r") as file:
         data = file.read()
         iterator = pattern.finditer(data)
+        do = True
         for match in iterator:
-            mul_tuple = eval(match.group()[3:])
-            sum += mul_tuple[0] * mul_tuple[1]
+            command = match.group()
+            if "do()" in command:
+                do = True
+                continue
+            if "don't()" in command:
+                do = False
+                continue
+            if do:
+                mul_tuple = eval(match.group()[3:])
+                sum += mul_tuple[0] * mul_tuple[1]
+
     print(sum)
 
 
