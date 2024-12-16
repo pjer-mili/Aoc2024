b3327e299cf4d018d32e16162144f7fa3c8fd6ac
PetarMilisavljevic <milisavljevicpetar6@gmail.com>
2024-12-16 10:34:37 +0100

    day 15 part 1

--- day_15/part_1.py
+++ day_15/part_1.py
@@ -7,7 +7,7 @@ class Direction(Enum):
     UP = ("^", (-1, 0))
     DOWN = ("v", (1, 0))
 
-    def __init__(self, symbol, delta):
+    def __init__(self, symbol: str, delta: tuple):
         self.symbol = symbol
         self.delta = delta
 
@@ -30,28 +30,8 @@ def get_robot_coordinates(matrix):
     return None
 
 
-def out_of_bounds(i, j, matrix):
-    return i < 0 or i >= len(matrix) or j < 0 or j >= len(matrix[0])
-
-
-def objects_num_to_move(i, j, direction: Direction, matrix: list[str]):
-    k = 1
-    dx, dy = direction.delta
-
-    while True:
-        new_i = i + k * dx
-        new_j = j + k * dy
-
-        if out_of_bounds(new_i, new_j, matrix):
-            return 0
-
-        current_cell = matrix[new_i][new_j]
-        if current_cell == "#":
-            return 0
-        if current_cell == ".":
-            return k
-
-        k += 1
+def in_bounds(i, j, matrix):
+    return 0 <= i < len(matrix) and 0 <= j < len(matrix[0])
 
 
 def print_matrix(matrix):
@@ -59,12 +39,47 @@ def print_matrix(matrix):
         print("".join(row))
 
 
+def calculate_gps(matrix):
+    gps = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] == "O":
+                gps += (100 * i) + j
+    return gps
+
+
 def main():
-    with open("test_input.txt", "r") as file:
+    with open("input.txt", "r") as file:
         m_data, dir_string = file.read().split("\n\n")
-        matrix = m_data.strip().split("\n")
-    directions = map_directions(dir_string)
+        matrix = [list(row) for row in m_data.strip().split("\n")]
     robot_i, robot_j = get_robot_coordinates(matrix)
+    directions = map_directions(dir_string)
+    for direction in directions:
+        dx, dy = direction.delta
+        n_i, n_j = robot_i + dx, robot_j + dy
+
+        if matrix[n_i][n_j] == "#":
+            continue
+
+        if matrix[n_i][n_j] == ".":
+            matrix[robot_i][robot_j] = "."
+            matrix[n_i][n_j] = "@"
+            robot_i, robot_j = n_i, n_j
+            continue
+
+        peek_i, peek_j = n_i + dx, n_j + dy
+        while matrix[peek_i][peek_j] != "." and matrix[peek_i][peek_j] != "#":
+            peek_i, peek_j = peek_i + dx, peek_j + dy
+
+        if matrix[peek_i][peek_j] == ".":
+            matrix[peek_i][peek_j] = "O"
+            matrix[robot_i][robot_j] = "."
+            matrix[n_i][n_j] = "@"
+            robot_i, robot_j = n_i, n_j
+
+    print_matrix(matrix)
+    gps = calculate_gps(matrix)
+    print(gps)
 
 
 if __name__ == "__main__":
